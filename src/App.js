75b5983002def3a28fb30b573f88a4e243dfe841
kmiller <kmmiller418@gmail.com>
2022-04-14 10:44:54 -0400

    remove unnecessary p elements from Content component

--- src/App.js
+++ src/App.js
@@ -11,15 +11,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        <Part part={props.part1} exercises={props.exercises1} />
-      </p>
-      <p>
-        <Part part={props.part2} exercises={props.exercises2} />
-      </p>
-      <p>
-        <Part part={props.part3} exercises={props.exercises3} />
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
